4387fac83a0c5da017ae21cc5a6fa38aad14a087
Jack Cooper <jackcooper1245@gmail.com>
2020-04-18 11:10:31 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -18,9 +18,9 @@ end
 def apply_discount
   if @discount != 0
     @total = (@total * 0.8)
- puts "After the discount, the total comes to #{@total}"
+ return "After the discount, the total comes to #{@total}"
  else
-   puts "There is no discount to apply!"
+   return "There is no discount to apply!"
   end
 end
 
