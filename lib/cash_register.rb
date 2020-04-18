807b581b27a3bfb298081743a309a1c9c43385e3
Jack Cooper <jackcooper1245@gmail.com>
2020-04-18 10:48:35 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,11 +1,11 @@
 
 class CashRegister
 
-  attr_accessor :employee_discount
+  attr_accessor :discount
 
 def initialize(employee_discount = nil)
   @total = 0
-  @employee_discount = employee_discount
+  @discount = employee_discount
 end
 
 end
